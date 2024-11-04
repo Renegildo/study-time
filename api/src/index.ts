985fe40933375e4e4c727da05d016b5e8619a989
renegildo <renegildo@proton.me>
2024-11-04 17:53:05 -0300

    initial commit

---
+++ api/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import router from "./router";
+dotenv.config();
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Api runnin on port ${port}`);
+});
