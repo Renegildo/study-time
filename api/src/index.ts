e07c9c20cc89430f5035a1f03536e4bffff90a75
renegildo <renegildo@proton.me>
2024-11-05 11:39:26 -0300

    feat: add cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./router";
+import cors from "cors";
 import { Timer } from "./timer";
 dotenv.config();
 
 const port = process.env.PORT || 8080;
 const app = express();
+
+app.use(cors());
 
 const timer = new Timer();
 app.use("/api", router(timer));
